d40de9fcd8ad9ab9a0f98ada1e3b29d2687ec725
ilhomxabibullayev <ilhomxabibullayev2004@gmail.com>
2024-09-22 12:49:53 +0500

    commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 // 1 masala
 
+for(let i = 1; i <= 50; i++) {
+    if(i % 2 == 0)
+    console.log(i)
+}
+
 // 2 masala
 
 // 3 masala
 
 // 4 masala
 
-for(let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
     console.log(Math.pow(i, 3))
 }
 
